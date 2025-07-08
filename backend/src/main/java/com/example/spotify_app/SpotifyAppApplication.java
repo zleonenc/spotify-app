09081e51dee1e24faaace07e267fc1549c377c2d
Zarif León <faustino.leon@encora.com>
2025-07-08 09:34:12 -0600

    chore: set up development environments with Docker for backend and frontend

---
+++ backend/src/main/java/com/example/spotify_app/SpotifyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotify_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/spotify_app/SpotifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotify_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
